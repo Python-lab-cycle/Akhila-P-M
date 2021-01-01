6beaee07a2edab37be623b6effbf3d905310498c
Akhilapm30 <76676530+Akhilapm30@users.noreply.github.com>
2021-01-01 08:59:12 +0530

    Add files via upload

---
+++ circlearea.py
@@ -0,0 +1,5 @@
+#Area of a Circle
+import math
+r=float(input("Enter the radius"))
+area=math.pi*r**2
+print("area=",area)
---
+++ greatestof3.py
@@ -0,0 +1,6 @@
+# Greatest of 3 Numbers
+a=int(input('enter first num'))
+b=int(input('enter second num'))
+c=int(input('enter third num'))
+big=max(a,b,c)
+print('largest is',big)
